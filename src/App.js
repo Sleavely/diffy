a823a98f1a689e5198b6de1efb127573c946216c
Joakim Hedlund <contact@joakimhedlund.com>
2020-03-18 09:18:35 +0100

    Simple DiffViewer implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import DiffViewer from './DiffViewer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Diffy</h1>
       </header>
+      <section style={{ paddingTop: '2em', paddingBottom: '10vh' }}>
+        <DiffViewer />
+      </section>
+      <footer>
+        <p>
+          Built and hosted by <a href="https://joakimhedlund.com">Joakim Hedlund</a>.
+          Powered by <a href="https://www.npmjs.com/package/react-diff-viewer" target="_blank" rel="noopener">react-diff-viewer</a>.
+        </p>
+      </footer>
     </div>
   );
 }
---
+++ src/DiffViewer.js
@@ -0,0 +1,65 @@
+import React, { PureComponent } from 'react';
+import ReactDiffViewer from 'react-diff-viewer';
+
+export default class DiffViewer extends PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      oldCode: '',
+      newCode: ''
+    };
+  }
+
+  render = () => {
+    return (
+      <div>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          width: '100%',
+        }}>
+          <textarea
+            placeholder="Old content"
+            onChange={(ev) => { this.setState({ oldCode: ev.currentTarget.value }) }}
+            cols="30"
+            rows="10"
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              flexBasis: '100%',
+              flex: '1',
+              marginLeft: '1em',
+              marginRight: '1em'
+            }}
+          ></textarea>
+          <textarea
+            placeholder="New content"
+            onChange={(ev) => { this.setState({ newCode: ev.currentTarget.value }) }}
+            cols="30"
+            rows="10"
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              flexBasis: '100%',
+              flex: '1',
+              marginLeft: '1em',
+              marginRight: '1em'
+            }}
+          ></textarea>
+        </div>
+        <div
+          style={{ margin: '1em' }}
+        >
+          <ReactDiffViewer
+            oldValue={this.state.oldCode}
+            newValue={this.state.newCode}
+            splitView={true}
+          //renderContent={this.highlightSyntax}
+          />
+        </div>
+      </div>
+    );
+  };
+}
